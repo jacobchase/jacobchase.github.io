a9015d145044055b97ac734776d345f9c8f51eca
jacobchase <jchase996@gmail.com>
2025-06-09 16:30:22 -0500

    created the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,14 @@
 export default function home(){
     return(
-        <h1>This is the home component</h1>
+        <main>
+            <div className="homeContainer">
+                <div className="homeBox">
+                    <div className="profileBox">
+                        <img src="src/assets/player.png" className="profile-picture"/>
+                        <p class="profile-description">Hi, I’m <span className="name">Jacob Chase</span>, a developer passionate about creating beautiful, responsive software.</p>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
