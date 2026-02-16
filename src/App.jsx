d31f3f98e5c8d3b0355833a96552bc96d56c9588
Jacob Chase <jchase996@gmail.com>
2026-02-16 16:28:32 -0600

    fresh start to code after archive

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/navbar'
-import Home from './pages/home'
-import Projects from './pages/projects'
-import Contact from './pages/contact'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <Router>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path='/projects' element ={<Projects/>} />
-      <Route path='contact' element ={<Contact/>}/>
-    </Routes>
-  </Router>
-  )
+    <>
+      <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <img class="size-12 shrink-0" src="/img/logo.svg" alt="ChitChat Logo" />
+        <div>
+          <div class="text-xl font-medium text-black dark:text-white">
+            ChitChat
+          </div>
+          <p class="text-gray-500 dark:text-gray-400">
+            You have a new message!
+          </p>
+        </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
 }
 
-export default App
+export default App;
